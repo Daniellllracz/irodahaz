3246250278237ff1ca448c139f7503d1c6a64d88
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2024-01-12 11:19:54 +0100

    Add project files.

---
+++ irodahaz/Iroda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irodahaz
+{
+    internal class Iroda
+    {
+        public string cegNev { get; set; }
+        public int kelletkez { get; set; }
+        private readonly List<int> letszamadat;
+
+
+        public Iroda(string adatok)
+        {
+            var v = adatok.Split(';');
+            cegNev = v[0];
+            kelletkez = int.Parse(v[1]);
+            letszamadat = new List<int>();
+        
+        
+        
+        
+        }
+        public override string ToString()
+        {
+            return $"Cégneve: {cegNev}\nkelletkezés : {kelletkez}\nletszám egy iroda házba : {letszamadat}";
+        }
+
+
+
+
+    }
+}
---
+++ irodahaz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace irodahaz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<StreamReader> irodaHaz = new();
+            using StreamReader sr = new(@"..\..\..\src\irodahaz.txt", Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) irodaHaz.Add(new(sr.ReadLine()));
+            sr.Close();
+
+            irodaHaz.ForEach(i => Console.WriteLine(i) );
+
+
+        }
+    }
+}
